1945eccd549998480ad6a8c224700104e31662d8
github <jhumphrey33@yahoo.com>
2021-10-13 20:06:01 -0500

    close to ag grid

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,19 +1,36 @@
 import { Component, OnInit } from "@angular/core";
-import { ColDef } from "ag-grid";
-import { RowDataTransaction } from "ag-grid-community";
+import { AgGridModule } from "ag-grid-angular";
 
 @Component({
   selector: "app-dashboard",
   templateUrl: "dashboard.component.html",
+  styleUrls: [ "dashboard.scss"]
 })
+
 export class DashboardComponent implements OnInit {
 
-  constructor() {}
+  columnDefs = 
+  [
+    {headerName: "Make", field: "make"},
+    {headerName: "Model", field: "model"},
+    {headerName: "Price", field: "price"}
+  ];
 
-  ngOnInit() {
+  rowData =
+  [
+    {make: 'Toyota', model: 'Celica', price: 35000 },
+    {make: 'Ford', model: 'Modeo', price: 32000 },
+    {make: 'Porche', model: 'Boxter', price: 72000 }
+  ]
+  constructor() 
+  {
     {
-      
+     
+
     }
-  
+
   }
+
+  ngOnInit() {}
+  
 }
