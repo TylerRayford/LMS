65db033d9fbf9a09760e29d2b631de608436f1c5
github <jhumphrey33@yahoo.com>
2021-10-18 20:13:37 -0500

    conflicts resolved

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,27 +1,14 @@
 import { Component, OnInit } from "@angular/core";
-import { AgGridModule } from "ag-grid-angular";
+
 
 @Component({
   selector: "app-dashboard",
-  templateUrl: "dashboard.component.html",
-  styleUrls: [ "dashboard.scss"]
+  templateUrl: "./dashboard.component.html",
 })
 
 export class DashboardComponent implements OnInit {
 
-  columnDefs = 
-  [
-    {headerName: "Make", field: "make"},
-    {headerName: "Model", field: "model"},
-    {headerName: "Price", field: "price"}
-  ];
-
-  rowData =
-  [
-    {make: 'Toyota', model: 'Celica', price: 35000 },
-    {make: 'Ford', model: 'Modeo', price: 32000 },
-    {make: 'Porche', model: 'Boxter', price: 72000 }
-  ]
+ 
   constructor() 
   {
     {
