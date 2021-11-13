33e7d0a969c41da098cd26ee82d40d3c327cebcc
tylerrayford <tylerrayford@gmail.com>
2021-11-12 19:17:31 -0600

    Editing commit

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 import { ColDef, Module } from 'ag-grid-community';
 import { AgGridAngular } from "ag-grid-angular";
-import { Observable } from "rxjs";
-import { HttpClient } from "@angular/common/http";
+import { catchError, Observable } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
 import { RangeSelectionModule } from "@ag-grid-enterprise/range-selection";
 
-
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
 
 function actionCellRenderer(params) {
   let eGui = document.createElement("div");
@@ -33,45 +37,7 @@ function actionCellRenderer(params) {
 
   return eGui;
 }
-/* function actionCellRenderer(params) {
-  let eGui = document.createElement("div");
 
-  let editingCells = params.api.getEditingCells();
-  // checks if the rowIndex matches in at least one of the editing cells
-  let isCurrentRowEditing = editingCells.some((cell) => {
-    return cell.rowIndex === params.node.rowIndex;
-  });
-
-  if (isCurrentRowEditing) {
-    eGui.innerHTML = `
-        <button  
-          class="action-button update"
-          data-action="update">
-               update  
-        </button>
-        <button  
-          class="action-button cancel"
-          data-action="cancel">
-               cancel
-        </button>
-        `;
-  } else {
-    eGui.innerHTML = `
-        <button 
-          class="action-button edit"  
-          data-action="edit">
-             edit 
-          </button>
-        <button 
-          class="action-button delete"
-          data-action="delete">
-             delete
-        </button>
-        `;
-  }
-
-  return eGui;
-} */
     
 @Component({
   selector: "dashboard",
@@ -84,11 +50,13 @@ export class DashboardComponent implements OnInit {
     public searchValue;
     public defaultColDef;
     public rowData: [];
+    private Id;
     
-  
+
 
 
   constructor(private http: HttpClient) {
+    
     
     
     
@@ -107,7 +75,7 @@ export class DashboardComponent implements OnInit {
       {
         headerName:"Client Name",
         field:"client_Name",
-        sortable: true,filter: true, resizable: true, checkboxSelection: true, /* editable:true */
+        sortable: true,filter: true, resizable: true, /* checkboxSelection: true *//* , editable:true */
       },
       {
         headerName:"Client Address",
@@ -191,6 +159,7 @@ export class DashboardComponent implements OnInit {
   }
   quickSearch(){
     this.gridApi.setQuickFilter(this.searchValue);
+    console.log('test')
   }
   onCellClicked(params) {
     // Handle click event for action cells
@@ -213,6 +182,7 @@ export class DashboardComponent implements OnInit {
 
       if (action === "update") {
         params.api.stopEditing(false);
+        
       }
 
       if (action === "cancel") {
@@ -220,6 +190,7 @@ export class DashboardComponent implements OnInit {
       }
     }
   }
+  
 
   onRowEditingStarted(params) {
     params.api.refreshCells({
@@ -229,12 +200,19 @@ export class DashboardComponent implements OnInit {
     });
   }
   onRowEditingStopped(params) {
+    this.gridApi=params.gridApi;
+      this.gridColumnApi=params.columnApi;
+      let id = params.data.id;
+      this.http.put<any>("https://localhost:44301/api/client/"+id, params.data, httpOptions).subscribe(/* data => this.Id = data.id */);
     params.api.refreshCells({
       columns: ["action"],
       rowNodes: [params.node],
-      force: true
+      force: true,
     });
+    setTimeout(
+      function(){ 
+      location.reload(); 
+      }, 1000);
   }
 }
   
-
